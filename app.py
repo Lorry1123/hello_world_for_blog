f109ff430eabb2ae102b4e982972cdbad72c8be7
Lorry <Lorry1123@outlook.com>
2019-07-21 21:48:37 +0800

    INIT

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+
+@app.route('/index')
+def index_page():
+    return 'this is a index page'
+
+if __name__ == '__main__':
+    app.run(port=80)
